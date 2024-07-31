97e5ba455d13033ec50937d2f9512b41be418e44
Kang-Minje <53532940+Kang-Minje@users.noreply.github.com>
2024-07-31 17:59:52 +0900

    Add files via upload

--- js/scrollscript.js
+++ js/scrollscript.js
@@ -3,8 +3,8 @@ window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
 
     if (scrollPosition === 0) {
-        box.style.width = '772px'; // 스크롤 위치가 0일 때 너비 836px
+        box.style.width = '852px'; // 스크롤 위치가 0일 때 너비 836px
     } else {
-        box.style.width = '860px'; // 스크롤 위치가 1 이상일 때 너비 860px
+        box.style.width = '910px'; // 스크롤 위치가 1 이상일 때 너비 860px
     }
 });
--- js/scrollscript_others.js
+++ js/scrollscript_others.js
@@ -1,11 +1,16 @@
 window.addEventListener('scroll', function() {
     const box = document.getElementById('box');
+    const prevPageBtn = document.getElementById('prevPageBtn');
+    const nextPageBtn = document.getElementById('nextPageBtn');
     const scrollPosition = window.scrollY;
 
     if (scrollPosition === 0) {
         box.style.width = '81.4vw'; // 스크롤 위치가 0일 때 너비 836px
+        prevPageBtn.style.display = 'none';
+        nextPageBtn.style.display = 'none';
     } else {
         box.style.width = '87vw'; // 스크롤 위치가 1 이상일 때 너비 860px
+        prevPageBtn.style.display = 'block';
+        nextPageBtn.style.display = 'block';
     }
 });
- 
